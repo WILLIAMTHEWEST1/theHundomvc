fd73ee68b0fdfa9062289290541a948214a6519b
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-10-28 09:28:42 -0500

    Add project files.

---
+++ theHundomvc/Controllers/HundoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using theHundomvc.Models;
+
+namespace theHundomvc.Controllers
+{
+    public class HundoController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(HundoNumbers nums)
+
+        {
+            List<string> numItems = new List<string>();
+
+            for (int i = nums.StartValue; i <= nums.EndValue; i++)
+            {
+                numItems.Add(i.ToString());
+            }
+
+            nums.Results = numItems;
+            return View(nums);
+        }
+
+        public IActionResult Index()
+        {
+            HundoNumbers nums = new HundoNumbers();
+
+            nums.StartValue = 1;
+            nums.EndValue = 100;
+
+
+            return View(nums);
+
+        }
+    }
+}
---
+++ theHundomvc/Models/HundoNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace theHundomvc.Models
+{
+    public class HundoNumbers
+    {
+        public int StartValue { get; set; }
+        public int EndValue { get; set; }
+        public List<string> Results { get; set; } = new List<string>();
+    }
+}
